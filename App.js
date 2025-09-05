4c208f10585c69c137b2367b0c9351db77a79b8c
LeandroGuidelli <leandrosoaresguidelli@gmail.com>
2025-09-05 20:30:49 -0300

    NEW: inserção de render e fetchUsers

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
-import api from './src/devices/api';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
+import api from "./src/devices/api";
 
-//declaração do componente principal 'App'
-export default App(){
+export default function App() {
   const [users, setUsers] = useState([]);
 
-  
+  async function fetchUsers() {
+    try {
+      const response = await api.get("/users"); 
+      setUsers(response.data);
+    } catch (error) {
+      console.error("Error GET:", error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const _render = () => {
+    const vet = [];
+    users.map((item, index) => {
+      vet.push(
+        <View key={index} style={styles.card}>
+          <Text style={styles.text}>ID: {item.id}</Text>
+          <Text style={styles.text}>Nome: {item.name}</Text>
+          <Text style={styles.text}>Email: {item.email}</Text>
+        </View>
+      );
+    });
+    return vet;
+  };
+
 }
--- src/devices/api.js
+++ src/devices/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const api = axios.create({
+    baseURL: "http://10.110.12.21:3000", // só o host e porta
     timeout:2000
 })
 export default api;
